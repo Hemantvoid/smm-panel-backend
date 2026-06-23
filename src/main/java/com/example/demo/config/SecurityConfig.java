bea1236343b5525b6f3855602f16ae3976a6d11d
Hemant Sahu <sahu74110@gmail.com>
2026-06-23 23:40:45 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.util.List;
+
 import com.example.demo.security.JwtFilter;
 import com.example.demo.security.OAuthSuccessHandler;
 
